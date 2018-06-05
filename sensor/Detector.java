b3903ea88602318233eef0afae6824bf40f7babd
ChataingT <38420068+ChataingT@users.noreply.github.com>
2018-06-05 14:14:16 +0200

    Modelisation of one sensor
    Interface with one sensor

---
+++ sensor/Detector.java
@@ -0,0 +1,37 @@
+package sensor;
+import java.util.List;
+
+import model.environment.Coord;
+
+
+
+/**
+ * Interface Detector
+ */
+public interface Detector {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /** Return the relative coordinate of the case detected
+   * @return  Map
+   */
+  public List<Coord> detection();
+
+
+}
